12090af8645f1e358f458fbf860c2a8c922d90dd
lucadisanto7 <lucadisanto47@gmail.com>
2024-06-24 14:54:36 +0200

    [FIX] Removed spaces between full infos

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ let first_name = prompt('Inserisci il tuo nome');
 let second_name = prompt('Inserisci il tuo cognome');
 let fav_color = prompt(' Inserisci il tuo colore preferito');
 
-let full_infos = `${first_name} ${second_name} ${fav_color}23`
+let full_infos = `${first_name}${second_name}${fav_color}23`
 console.log(full_infos)
 document.getElementById('full_infos').innerHTML = full_infos
