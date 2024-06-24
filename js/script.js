57d95820515715fe14078a1c0dd2c302fce33f38
lucadisanto7 <lucadisanto47@gmail.com>
2024-06-24 14:45:26 +0200

    [UPD] Added js and full infos id

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let first_name = prompt('Inserisci il tuo nome');
+let second_name = prompt('Inserisci il tuo cognome');
+let fav_color = prompt(' Inserisci il tuo colore preferito');
+
+let full_infos = `${first_name} ${second_name} ${fav_color}23`
+console.log(full_infos)
+document.getElementById('full_infos').innerHTML = full_infos
